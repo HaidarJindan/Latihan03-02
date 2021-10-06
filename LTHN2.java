99aaf2ceadd724fbe7792bbe848414c885a094c0
Haidar341 <91077398+Haidar341@users.noreply.github.com>
2021-10-06 14:05:41 +0700

    LTHN2.java

---
+++ LTHN2.java
@@ -0,0 +1,15 @@
+public class LTHN2{
+    public static void main(String[] args){
+	     int pengeluaran = 0;
+		 pengeluaran = pengeluaran + 2500000;
+		 pengeluaran = pengeluaran - 100000;
+		 pengeluaran = pengeluaran - 85000;
+		 pengeluaran = pengeluaran - 101000;
+		 pengeluaran = pengeluaran - 120000;
+		 pengeluaran = pengeluaran - 50000;
+		 pengeluaran = pengeluaran - 75000;
+		  
+		 System.out.println("Sisa uang: " + pengeluaran);
+		 
+	}
+}	
